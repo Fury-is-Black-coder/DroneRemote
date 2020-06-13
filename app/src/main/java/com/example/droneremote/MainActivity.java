83e0629ddcc967bb3cb6cdca37bb8e1ac5d83672
Fury-is-Black-coder <57364788+Fury-is-Black-coder@users.noreply.github.com>
2020-06-13 02:33:40 +0200

    Screen for adding device

--- app/src/main/java/com/example/droneremote/MainActivity.java
+++ app/src/main/java/com/example/droneremote/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.droneremote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.OutputStream;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // Declaration
+    private static final String TAG = "bluetooth1";
+    Button btnOn, btnOff;
+    private static final int REQUEST_ENABLE_BT = 1;
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private OutputStream outStream = null;
+
+    // SPP UUID сервиса
+    private static final UUID MY_UUID = UUID.fromString("0000FFE0-0000-1000-8000-00805F9B34FB");
+
+    // MAC-адрес Bluetooth модуля
+    private static String address = "88:25:83:F0:0F:EA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnConectionToBle).setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int i = view.getId();
+        if (i == R.id.btnConectionToBle){
+            startActivity(new Intent(MainActivity.this, Remote.class));
+        }
     }
 }
---
+++ app/src/main/java/com/example/droneremote/Remote.java
@@ -0,0 +1,14 @@
+package com.example.droneremote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Remote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.remote);
+    }
+}
