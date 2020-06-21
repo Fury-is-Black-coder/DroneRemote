286874372c631d507cc32814bb5943c08ba055a0
Fury-is-Black-coder <57364788+Fury-is-Black-coder@users.noreply.github.com>
2020-06-21 16:17:20 +0200

    Screen for adding device

--- app/src/main/java/com/example/droneremote/MainActivity.java
+++ app/src/main/java/com/example/droneremote/MainActivity.java
@@ -14,27 +14,12 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    // Declaration
-    private static final String TAG = "bluetooth1";
-    Button btnOn, btnOff;
-    private static final int REQUEST_ENABLE_BT = 1;
-    private BluetoothAdapter btAdapter = null;
-    private BluetoothSocket btSocket = null;
-    private OutputStream outStream = null;
-
-    // SPP UUID сервиса
-    private static final UUID MY_UUID = UUID.fromString("0000FFE0-0000-1000-8000-00805F9B34FB");
-
-    // MAC-адрес Bluetooth модуля
-    private static String address = "88:25:83:F0:0F:EA";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.btnConectionToBle).setOnClickListener(this);
-
 
     }
 
--- app/src/main/java/com/example/droneremote/Remote.java
+++ app/src/main/java/com/example/droneremote/Remote.java
@@ -2,13 +2,171 @@ package com.example.droneremote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Remote extends AppCompatActivity {
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
+
+public class Remote extends AppCompatActivity implements View.OnClickListener {
+
+    // Declaration
+    private static final String TAG = "bluetooth1";
+    Button btnOn, btnOff;
+    private static final int REQUEST_ENABLE_BT = 1;
+    private BluetoothAdapter mBluetoothAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private OutputStream outStream = null;
+
+    // SPP UUID сервиса
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    // MAC-адрес Bluetooth модуля
+    private static String address = "88:25:83:F0:14:AC";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.remote);
+
+        findViewById(R.id.btnOn).setOnClickListener(this);
+        findViewById(R.id.btnOff).setOnClickListener(this);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        checkBTState();
+
     }
+
+    @Override
+    public void onClick(View view) {
+
+        int i = view.getId();
+        if (i == R.id.btnOn){
+            sendData("1");
+            Toast.makeText(getBaseContext(), "Включаем LED", Toast.LENGTH_SHORT).show();
+        }
+
+        if (i == R.id.btnOff){
+            sendData("0");
+            Toast.makeText(getBaseContext(), "Выключаем LED", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d(TAG, "...onResume - попытка соединения...");
+
+        // Set up a pointer to the remote node using it's address.
+        BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+
+        // Two things are needed to make a connection:
+        //   A MAC address, which we got above.
+        //   A Service ID or UUID.  In this case we are using the
+        //     UUID for SPP.
+        try {
+            btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+        } catch (IOException e) {
+            errorExit("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
+        }
+
+        // Discovery is resource intensive.  Make sure it isn't going on
+        // when you attempt to connect and pass your message.
+        mBluetoothAdapter.cancelDiscovery();
+
+        // Establish the connection.  This will block until it connects.
+        Log.d(TAG, "...Соединяемся...");
+        try {
+            btSocket.connect();
+            Log.d(TAG, "...Соединение установлено и готово к передачи данных...");
+        } catch (IOException e) {
+            try {
+                btSocket.close();
+            } catch (IOException e2) {
+                errorExit("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
+            }
+        }
+
+        // Create a data stream so we can talk to server.
+        Log.d(TAG, "...Создание Socket...");
+
+        try {
+            outStream = btSocket.getOutputStream();
+        } catch (IOException e) {
+            errorExit("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d(TAG, "...In onPause()...");
+
+        if (outStream != null) {
+            try {
+                outStream.flush();
+            } catch (IOException e) {
+                errorExit("Fatal Error", "In onPause() and failed to flush output stream: " + e.getMessage() + ".");
+            }
+        }
+
+        try     {
+            btSocket.close();
+        } catch (IOException e2) {
+            errorExit("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
+        }
+    }
+
+    private void checkBTState() {
+        // Check for Bluetooth support and then check to make sure it is turned on
+        // Emulator doesn't support Bluetooth and will return null
+        if(mBluetoothAdapter ==null) {
+            errorExit("Fatal Error", "Bluetooth не поддерживается");
+        } else {
+            if (mBluetoothAdapter.isEnabled()) {
+                Log.d(TAG, "...Bluetooth включен...");
+            } else {
+                //Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(mBluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
+    }
+
+    private void errorExit(String title, String message){
+        Toast.makeText(getBaseContext(), title + " - " + message, Toast.LENGTH_LONG).show();
+        finish();
+    }
+
+    private void sendData(String message) {
+        byte[] msgBuffer = message.getBytes();
+
+        Log.d(TAG, "...Посылаем данные: " + message + "...");
+
+        try {
+            outStream.write(msgBuffer);
+        } catch (IOException e) {
+            String msg = "In onResume() and an exception occurred during write: " + e.getMessage();
+            if (address.equals("00:00:00:00:00:00"))
+                msg = msg + ".\n\nВ переменной address у вас прописан 00:00:00:00:00:00, вам необходимо прописать реальный MAC-адрес Bluetooth модуля";
+                msg = msg +  ".\n\nПроверьте поддержку SPP UUID: " + MY_UUID.toString() + " на Bluetooth модуле, к которому вы подключаетесь.\n\n";
+
+                errorExit("Fatal Error", msg);
+        }
+    }
+
+
+
 }
